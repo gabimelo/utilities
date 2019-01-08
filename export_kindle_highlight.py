95ab6e63e6c08be3495f38de21efd53478fa8bd2
Gabriela Melo <gabimelo@users.noreply.github.com>
2019-01-08 21:53:26 -0200

    Add files via upload

---
+++ export_kindle_highlight.py
@@ -0,0 +1,24 @@
+with open('My Clippings.txt', 'r', encoding='utf-8-sig') as f:
+    content = f.read().splitlines() 
+
+book_clippings = {}
+
+current_book = None
+
+for line in content:
+    if line is None or line == '':
+        pass
+    elif current_book is None:
+        current_book = line.replace('\ufeff', '')
+    elif line == '==========':
+        current_book = None
+    else:
+        if current_book in book_clippings:
+            book_clippings[current_book] += line + '\n'
+        else:
+            book_clippings[current_book] = line + '\n'
+
+for key, value in book_clippings.items():
+    if key != 'my_book':
+        with open('{}.txt'.format(key), 'w') as f:
+            f.write(value)
