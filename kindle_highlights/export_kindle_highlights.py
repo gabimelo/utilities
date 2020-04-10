008dfd113c85e94c21172a4bc71c8a7d2adc91c7
Gabriela Melo <gabimelo10@gmail.com>
2020-04-10 13:28:52 -0300

    organize kindle highlights code

--- kindle_highlights/export_kindle_highlights.py
+++ kindle_highlights/export_kindle_highlights.py
@@ -1,29 +1,39 @@
 import os
 
-if not os.path.exists('books'):
-    os.makedirs('books')
 
-with open('My Clippings.txt', 'r', encoding='utf-8-sig') as f:
-    content = f.read().splitlines() 
+def main():
+    with open('My Clippings.txt', 'r', encoding='utf-8-sig') as f:
+        content = f.read().splitlines()
+    book_clippings = get_book_clippings_dict(content)
+    write_clippings(book_clippings)
 
-book_clippings = {}
 
-current_book = None
+def write_clippings(book_clippings):
+    if not os.path.exists('books'):
+        os.makedirs('books')
+    for key, value in book_clippings.items():
+        if key != 'my_book':
+            with open('books/{}.txt'.format(key), 'w') as f:
+                f.write(value)
 
-for line in content:
-    if line is None or line == '':
-        pass
-    elif current_book is None:
-        current_book = line.replace('\ufeff', '')
-    elif line == '==========':
-        current_book = None
-    else:
-        if current_book in book_clippings:
-            book_clippings[current_book] += line + '\n'
+
+def get_book_clippings_dict(content):
+    book_clippings = {}
+    current_book = None
+    for line in content:
+        if line is None or line == '':
+            pass
+        elif current_book is None:
+            current_book = line.replace('\ufeff', '')
+        elif line == '==========':
+            current_book = None
         else:
-            book_clippings[current_book] = line + '\n'
+            if current_book in book_clippings:
+                book_clippings[current_book] += line + '\n'
+            else:
+                book_clippings[current_book] = line + '\n'
+    return book_clippings
 
-for key, value in book_clippings.items():
-    if key != 'my_book':
-        with open('books/{}.txt'.format(key), 'w') as f:
-            f.write(value)
+
+if __name__ == '__main__':
+    main()
