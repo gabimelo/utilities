8e19c727ae03c31092c231c3e76455a0f55d4ecd
Gabriela Melo <gabimelo10@gmail.com>
2020-04-10 13:25:28 -0300

    don't modify dfs in place

--- data_science_utils/utils.py
+++ data_science_utils/utils.py
@@ -5,34 +5,36 @@ def string_br_monetary_series_to_float(series):
     return series.str.replace(',', '.').astype(float)
 
 
-def generate_date_and_time_columns(df):
-    df['month'] = df.datetime.apply(lambda x: x.month)
-    df['day'] = df.datetime.apply(lambda x: x.day)
-    df['day_of_week'] = df.datetime.apply(lambda x: x.dayofweek)  # day of the week with Monday=0, Sunday=6
-    df['hour'] = df.datetime.apply(lambda x: x.hour)
-
-    return df
-
-
-def filter_df(df, column_name, column_value):
-    return df[df[column_name] == column_value]
+def generate_date_and_time_columns(df_in):
+    df_out = df_in.copy()
+    df_out['month'] = df_out.datetime.apply(lambda x: x.month)
+    df_out['day'] = df_out.datetime.apply(lambda x: x.day)
+    df_out['day_of_week'] = df_out.datetime.apply(lambda x: x.dayofweek)  # day of the week with Monday=0, Sunday=6
+    df_out['hour'] = df_out.datetime.apply(lambda x: x.hour)
+    return df_out
 
 
-def turn_columns_to_categorical(df, column_list):
+def filter_df(df_in, column_name, column_value):
+    df_out = df_in.copy()
+    return df_out[df_out[column_name] == column_value]
+
+
+def turn_columns_to_categorical(df_in, column_list):
+    df_out = df_in.copy()
     for col in column_list:
         try:
-            df[col] = pd.Categorical(df[col])
+            df_out[col] = pd.Categorical(df_out[col])
         except KeyError:
             pass
-    return df
+    return df_out
 
 
-def remove_unused_categories_in_df(df):
-    cat_cols = df.select_dtypes(include='category').columns
+def remove_unused_categories_in_df(df_in):
+    df_out = df_in.copy()
+    cat_cols = df_out.select_dtypes(include='category').columns
     for col in cat_cols:
-        df[col] = df[col].cat.remove_unused_categories()
-
-    return df
+        df_out[col] = df_out[col].cat.remove_unused_categories()
+    return df_out
 
 
 def get_list_of_missing_columns_in_df(df, required_cols):
